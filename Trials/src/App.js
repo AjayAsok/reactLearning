3e072472243ca92b25cf4fbbcc0d052282deee6a
AjayAsok <ajayasok007@gmail.com>
2020-05-19 04:40:25 +0530

    Adding more component classes

--- Trials/src/App.js
+++ Trials/src/App.js
@@ -2,10 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    "Hello React World!!"
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      answer: "yes"
+    }
+  }
+  textGenerator() {
+    return "Hello React World!!"
+  }
+
+  render() {
+    return (<div>
+      <LoginComponent />
+      <h1>Is the state important? {this.state.answer} </h1>
+      <Header userName="user" />
+    </div>
+    )
+
+  }
+}
+class Header extends React.Component {
+  render() {
+    return (<header>
+      <p>Welcome, {this.props.userName}</p>
+    </header>)
+  }
+}
+
+class LoginComponent extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      isLoggedIn: false
+    }
+  }
+  render() {
+    return (<h1>You are Currently logged{this.state.isLoggedIn ? "in" : "out"}</h1>)
+  }
 }
 
 export default App;
