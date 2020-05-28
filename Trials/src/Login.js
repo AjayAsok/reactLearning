833384f53ca26130fa696d8f417f406863b78722
AjayAsok <ajayasok007@gmail.com>
2020-05-29 04:20:58 +0530

    React forms

--- Trials/src/Login.js
+++ Trials/src/Login.js
@@ -5,13 +5,23 @@ class Login extends React.Component {
     constructor() {
         super()
         this.state = {
-            isLoggedIn: true
+            isLoggedIn: true,
+            responseData: {}
         }
         this.handleLogin = this.handleLogin.bind(this)
     }
+    componentDidMount() {
+        this.responseData = fetch("https://swapi.dev/api/people/1").then(response => response.json())
+            .then(responseData => {
+                this.setState({
+                    responseData: responseData
+                })
+            })
+        console.log(this.responseData)
+    }
     render() {
         return (<div>
-            <h1>You are Currently logged{this.state.isLoggedIn ? " in" : " out"}</h1>
+            <h1>You are Currently logged{this.state.isLoggedIn ? " in " + this.state.responseData.name : " out"}</h1>
             <button onClick={this.handleLogin}>{this.state.isLoggedIn ? "Logout" : "Login"}</button>
         </div>
         )
