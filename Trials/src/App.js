49ee435e71698e8bb6f94023ad8d265eb8623b30
AjayAsok <ajayasok007@gmail.com>
2020-06-06 17:20:26 +0530

    changes as part of linkedin learning

--- Trials/src/App.js
+++ Trials/src/App.js
@@ -1,60 +1,116 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import Conditional from "./Conditional"
-import Login from "./Login"
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      isLoading: true,
-      answer: "yes",
-      count: 0,
-      unreadMsgs: ["This is the first msg", " ", "This is a second msg"],
-      isLoggedIn: true
-    }
-    this.changeValue = this.changeValue.bind(this)
-  }
-  textGenerator() {
-    return "Hello React World!!"
-  }
-  changeValue() {
-    this.setState(prevState => {
-      return {
-        count: prevState.count + 1
-      }
-    })
-  }
-  componentDidMount() {
-    setTimeout(() => {
+// import Conditional from "./Conditional"
+// import Login from "./Login"
 
-      this.setState({
-        isLoading: false
-      })
+const cityList = ["Cochin", "London", "Manchester"];
+function App() {
+  return (
 
-    }, 1000)
-  }
-  render() {
-    return (<div>
-      <Login />
-      <h1>{this.state.unreadMsgs && this.state.unreadMsgs}</h1>
-      <Conditional isLoading={this.state.isLoading} />
-      <h1>{this.state.count}</h1>
-      <button onClick={this.changeValue}>Change!</button>
-      <hr /><hr />
-      <h1>Is the state important? {this.state.answer} </h1>
-      <Header userName="user" />
-    </div>
-    )
+    <>
 
-  }
+      <City cities={cityList} />
+      <Fragment />
+      <StateChanger />
+    </>)
 }
-class Header extends React.Component {
-  render() {
-    return (<header>
-      <p>Welcome, {this.props.userName}</p>
-    </header>)
-  }
+
+function City({ cities }) {
+  return (<ul>
+    {cities.map(city => (
+      <li key={city}>{city}</li>
+    ))}
+  </ul>)
 }
+
+function Fragment() {
+  const [first, , third] = [1, 2, 3]
+  return <h2>This is a fragment{first + " " + third}</h2>
+}
+
+//Use state Hook
+
+function StateChanger() {
+  const [status, setStatus] = useState("Open");
+  useEffect(() => {
+    alert(`status: ${status}`)
+  }, [status])
+  const [val1, setVal1] = useState("")
+  const [val2, setVal2] = useState("")
+
+  useEffect(() => {
+    alert(val1)
+  }, [val1])
+  useEffect(() => {
+    alert(val2)
+  }, [val2])
+  return <>
+    Here You can Change the Status
+    <h3>Status: {status}</h3>
+    val1
+    <input value={val1} onChange={e => setVal1(e.target.value)} />
+    <br />
+    val2
+    <input value={val2} onChange={e => setVal2(e.target.value)} />
+
+    <button onClick={() => setStatus("Closed")}>Close</button>
+    <button onClick={() => setStatus("Open")}>Open</button>
+  </>
+}
+
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       isLoading: true,
+//       answer: "yes",
+//       count: 0,
+//       unreadMsgs: ["This is the first msg", " ", "This is a second msg"],
+//       isLoggedIn: true
+//     }
+//     this.changeValue = this.changeValue.bind(this)
+//   }
+//   textGenerator() {
+//     return "Hello React World!!"
+//   }
+//   changeValue() {
+//     this.setState(prevState => {
+//       return {
+//         count: prevState.count + 1
+//       }
+//     })
+//   }
+//   componentDidMount() {
+//     setTimeout(() => {
+
+//       this.setState({
+//         isLoading: false
+//       })
+
+//     }, 1000)
+//   }
+//   render() {
+//     return (<div>
+//       <Login />
+//       <h1>{this.state.unreadMsgs && this.state.unreadMsgs}</h1>
+//       <Conditional isLoading={this.state.isLoading} />
+//       <h1>{this.state.count}</h1>
+//       <button onClick={this.changeValue}>Change!</button>
+//       <hr /><hr />
+//       <h1>Is the state important? {this.state.answer} </h1>
+//       <Header userName="user" />
+//     </div>
+//     )
+
+//   }
+// }
+// class Header extends React.Component {
+//   render() {
+//     return (<header>
+//       <p>Welcome, {this.props.userName}</p>
+//     </header>)
+//   }
+// }
 
 
 export default App;
--- Trials/src/index.js
+++ Trials/src/index.js
@@ -4,9 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(React.createElement('div', { style: { color: "red" } },
+    React.createElement('h2', null, "I am a child")),
+    document.getElementById('my-sub'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'));
